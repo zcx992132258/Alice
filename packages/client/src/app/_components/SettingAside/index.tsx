8c69176ed12a96be5e16fcfbf64d4c7c51d4b321
chenjiaying <1935227706@qq.com>
2023-09-11 16:04:32 +0800

    feat：关闭按钮

--- packages/client/src/app/_components/SettingAside/index.tsx
+++ packages/client/src/app/_components/SettingAside/index.tsx
@@ -1,28 +1,32 @@
-'use client'
-import { usePageContext } from '../Context'
-import { style } from '../'
-import { CloseCircleOutlined, Sider } from '@/lib/Antd'
+'use client';
+import { usePageContext } from '../Context';
+import { style } from '../';
+import { CloseCircleOutlined, Sider } from '@/lib/Antd';
 
 export function SettingAside() {
-  const { settingCollapsed, curComponent } = usePageContext()
-  return (
-    <Sider
-      className={style.settingAside}
-      collapsed={settingCollapsed}
-      width={300}
-      collapsedWidth={0}
-      style={{
-        background: '#ffffff',
-        borderLeft: '1px solid #e7ecef',
-      }}
-    >
-      <div className='flex justify-between items-center  px-[20px] h-[40px] text-[12px]'>
-        <span> {curComponent?.name}</span>
-        <CloseCircleOutlined className='text-[18px]' />
-      </div>
-      <div className={style.wrap} />
-    </Sider>
-  )
+    const { settingCollapsed, curComponent, setSettingCollapsed } =
+        usePageContext();
+    return (
+        <Sider
+            className={style.settingAside}
+            collapsed={settingCollapsed}
+            width={300}
+            collapsedWidth={0}
+            style={{
+                background: '#ffffff',
+                borderLeft: '1px solid #e7ecef',
+            }}
+        >
+            <div className="flex justify-between items-center  px-[20px] h-[40px] text-[12px]">
+                <span> {curComponent?.name}</span>
+                <CloseCircleOutlined
+                    className="text-[18px]"
+                    onClick={() => setSettingCollapsed(true)}
+                />
+            </div>
+            <div className={style.wrap} />
+        </Sider>
+    );
 }
 
-export default SettingAside
+export default SettingAside;
