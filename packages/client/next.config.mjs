abcb2631e51861464fa621db2e9e35e4ee982102
最强猎马人 <54138798+zcx992132258@users.noreply.github.com>
2024-03-01 01:20:11 +0800

    feat:配置文件修改

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -2,6 +2,7 @@ import million from 'million/compiler'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   experimental: {
     turbo: {
